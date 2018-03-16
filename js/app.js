b99661c4026c102bf3b8195a2d14e08ce6a8ce34
Dylan Ellison <dylancellison@gmail.com>
2018-03-15 17:30:32 -0700

    added several markers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 var map;
+
+var markers = [];
+
 function initMap() {
     //Constructor creates a new map - only center and zoom are required
     map = new google.maps.Map(document.getElementById('map'), {
@@ -86,16 +89,76 @@ function initMap() {
         ],
         mapTypeControl: false
     });
-    var tribeca =  {lat: 34.001201, lng: -118.806442};
-    var marker = new google.maps.Marker({
-        position: tribeca,
-        map: map,
-        title: 'Marker!!!'
-    });
-    var infowindow = new google.maps.InfoWindow({
-        content: 'This is my favorite surf spot'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map,marker);
-    });
+
+    // Marker Locations
+    var locations = [
+        {title: 'Pt Dume', location: {lat: 34.001201,lng: -118.806442}},
+        {title: 'Leo Carillo', location: {lat: 34.044551,lng:-118.940695}},
+        {title: 'County Line', location: {lat: 34.051425,lng: -118.95996}},
+        {title: 'Malibu Pt', location: {lat: 34.036265 ,lng: -118.67795}},
+        {title: 'Westward Beach', location: {lat: 34.008341 ,lng: -118.814812}}
+    ];
+
+    var largeInfowindow = new google.maps.InfoWindow();
+
+    var bounds = new google.maps.LatLngBounds();
+
+    // creates array of markers
+
+    for (var i = 0; i < locations.length; i++) {
+
+        var position = locations[i].location;
+        var title = locations[i].title
+
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+
+        markers.push(marker);
+
+        bounds.extend(marker.position);
+
+        marker.addListener('click', function(){
+            populateInfoWindow(this,largeInfowindow);
+        });
+    }
+    map.fitBounds(bounds);
+
+    // populates info window whem marker is clicked.
+    function populateInfoWindow(marker, infowindow){
+        if (infowindow.marker != marker){
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.open(map, marker);
+            // clear marker property if info window closed
+            infowindow.addListener('closeclick', function(){
+                infowindow.setMarker(null);
+            })
+        }
+
+    }
+
 }
+
+
+
+
+
+/*/ create marker
+var tribeca =  {lat: 34.001201, lng: -118.806442};
+var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'Pt Dume'
+});
+var infowindow = new google.maps.InfoWindow({
+    content: 'This is my favorite surf spot'
+});
+marker.addListener('click', function() {
+    infowindow.open(map,marker);
+});
+*/
