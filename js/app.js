18c5d223fc60ceff388e693dd9a7ed7a31510530
Dylan Ellison <dylancellison@gmail.com>
2018-03-21 18:03:12 -0700

    reformatted main script

--- js/app.js
+++ js/app.js
@@ -109,9 +109,9 @@ function initMap() {
     // Create a "highlighted location" marker color for when the user
     // mouses over the marker.
     var highlightedIcon = makeMarkerIcon('FFFF24');
-///////
+    ///////
     //var bounds = new google.maps.LatLngBounds();
-///////
+    ///////
     // creates array of markers
 
     for (var i = 0; i < locations.length; i++) {
@@ -149,9 +149,9 @@ function initMap() {
        document.getElementById('hide-listings').addEventListener('click', hideListings);
 
 }
-//    map.fitBounds(bounds);
-
+    // map.fitBounds(bounds);
     // populates info window whem marker is clicked.
+
 function populateInfoWindow(marker, infowindow){
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
@@ -185,15 +185,13 @@ function populateInfoWindow(marker, infowindow){
             else {
                 infowindow.setContent('<div>No Street View Found</div>');
             }
-    }
+        }
     // use street to get cloest streetview image within
     // 50 meters of teh markers position
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
     //open info window on correct marker
     infowindow.open(map, marker);
-}
-
-
+    }
 }
 
 var ViewModel = function(){
@@ -203,31 +201,31 @@ var ViewModel = function(){
 }
 
 // This function will loop through the markers array and display them all.
-      function showListings() {
-        var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-      }
-      // This function will loop through the listings and hide them all.
-      function hideListings() {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-        }
-      }
-      // This function takes in a COLOR, and then creates a new marker
-      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-      // of 0, 0 and be anchored at 10, 34).
-      function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-      }
+function showListings() {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+    }
+    map.fitBounds(bounds);
+}
+// This function will loop through the listings and hide them all.
+function hideListings() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+}
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+    return markerImage;
+}
