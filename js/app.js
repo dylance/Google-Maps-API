abb53c1b1274c179508a2d1343e45fe57b947714
Dylan Ellison <dylancellison@gmail.com>
2018-04-01 17:39:16 -0700

    applied atom-beautify for formatting

--- js/app.js
+++ js/app.js
@@ -1,149 +1,153 @@
 let map;
-let surfSpots = [
-    {title: 'Pt Dume',
-     location: {lat: 34.001201,lng: -118.806442},
-     id: "854"},
-    {title: 'Leo Carillo',
-    location: {lat: 34.044551,lng:-118.940695},
-    id: "2642"},
-    {title: 'County Line',
-    location: {lat: 34.051425,lng: -118.95996},
-    id: "277"},
-    {title: 'Malibu Pt',
-    location: {lat: 34.036265 ,lng: -118.67795},
-    id: "279"},
-    {title: 'Westward Beach',
-    location: {lat: 34.008341 ,lng: -118.814812},
-    id: "2610"}
+let surfSpots = [{
+        title: 'Pt Dume',
+        location: {
+            lat: 34.001201,
+            lng: -118.806442
+        },
+        id: "854"
+    },
+    {
+        title: 'Leo Carillo',
+        location: {
+            lat: 34.044551,
+            lng: -118.940695
+        },
+        id: "2642"
+    },
+    {
+        title: 'County Line',
+        location: {
+            lat: 34.051425,
+            lng: -118.95996
+        },
+        id: "277"
+    },
+    {
+        title: 'Malibu Pt',
+        location: {
+            lat: 34.036265,
+            lng: -118.67795
+        },
+        id: "279"
+    },
+    {
+        title: 'Westward Beach',
+        location: {
+            lat: 34.008341,
+            lng: -118.814812
+        },
+        id: "2610"
+    }
 ]
-let mSWurl = "http://magicseaweed.com/api/c601eb5d859031e96bd33e9f0ea25b26/forecast/?spot_id=";
+let mSWurl = 'http://magicseaweed.com/api/c601eb5d859031e96bd33e9f0ea25b26/forecast/?spot_id=';
+
 function initMap() {
     //Constructor creates a new map - only center and zoom are required
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 34.001201, lng: -118.806442},
+        center: {
+            lat: 34.001201,
+            lng: -118.806442
+        },
         zoom: 12,
-        styles:
-        [
-          {
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#fffec4"
-              }
-            ]
-          },
-          {
-            "featureType": "administrative.land_parcel",
-            "elementType": "labels",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "landscape",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "visibility": "on"
-              }
-            ]
-          },
-          {
-            "featureType": "landscape.natural.terrain",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#65a75e"
-              },
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "poi",
-            "elementType": "labels.text",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#a75808"
-              }
-            ]
-          },
+        styles: [{
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#fffec4"
+                }]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "visibility": "on"
+                }]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                        "color": "#65a75e"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#a75808"
+                }]
+            },
 
-          {
-            "featureType": "road.local",
-            "elementType": "labels",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "road.local",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#247212"
-              }
-            ]
-          },
-          {
-            "featureType": "transit",
-            "stylers": [
-              {
-                "visibility": "off"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "geometry.fill",
-            "stylers": [
-              {
-                "color": "#32e8ff"
-              },
-              {
-                "weight": 4.5
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text",
-            "stylers": [
-              {
-                "color": "#a305ff"
-              }
-            ]
-          },
-          {
-            "featureType": "water",
-            "elementType": "labels.text.stroke",
-            "stylers": [
-              {
-                "color": "#fcff00"
-              }
-            ]
-          }
+            {
+                "featureType": "road.local",
+                "elementType": "labels",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                    "color": "#247212"
+                }]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [{
+                    "visibility": "off"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [{
+                        "color": "#32e8ff"
+                    },
+                    {
+                        "weight": 4.5
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text",
+                "stylers": [{
+                    "color": "#a305ff"
+                }]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [{
+                    "color": "#fcff00"
+                }]
+            }
         ],
-    mapTypeControl: false
+        mapTypeControl: false
     });
     ko.applyBindings(new ViewModel())
-};
+}
 
-let surfSpot = function(data){
+let surfSpot = function(data) {
     self = this;
 
     this.name = data.title;
@@ -163,20 +167,20 @@ let surfSpot = function(data){
     // mouses over the marker.
     let highlightedIcon = makeMarkerIcon('e80000');
     this.marker = new google.maps.Marker({
-            position: this.position,
-            title: this.name,
-            animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
+        position: this.position,
+        title: this.name,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
     });
 
     magCopy(self.url, this)();
 
     let largeInfowindow = new google.maps.InfoWindow();
 
-    this.marker.addListener('click',( function(markerCopy){
-            return function() {
-               populateInfoWindow(this,largeInfowindow, markerCopy);
-           }
+    this.marker.addListener('click', (function(markerCopy) {
+        return function() {
+            populateInfoWindow(this, largeInfowindow, markerCopy);
+        }
     })(self));
     // Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
@@ -190,53 +194,51 @@ let surfSpot = function(data){
     });
 }
 
-let ViewModel = function(){
+let ViewModel = function() {
     let self = this;
 
     this.s = ko.observable('show spots!');
     this.d = ko.observable('hide spots!');
-    this.filter =  ko.observable('');
+    this.filter = ko.observable('');
 
     this.spotList = ko.observableArray([]);
 
-    surfSpots.forEach(function(spot){
+    surfSpots.forEach(function(spot) {
         self.spotList.push(new surfSpot(spot));
     })
 
-    this.showSpot = function(clickedSpot){
+    this.showSpot = function(clickedSpot) {
         clickedSpot.marker.setMap(map);
         map.setZoom(15);
         map.panTo(clickedSpot.position);
     }
     // This function will loop through the markers array and display them all.
-    this.showListings = function(){
+    this.showListings = function() {
         let bounds = new google.maps.LatLngBounds();
         // Extend the boundaries of the map for each marker and display the marker
-        self.spotList().forEach(function(spot){
+        self.spotList().forEach(function(spot) {
             spot.marker.setMap(map);
             bounds.extend(spot.position);
         })
         map.fitBounds(bounds);
     }
     // This function will loop through the listings and hide them all.
-    this.hideListings = function(){
-        self.spotList().forEach(function(spot){
+    this.hideListings = function() {
+        self.spotList().forEach(function(spot) {
             spot.marker.setMap(null);
         })
     }
 
-    this.filteredItems = ko.computed(function(){
+    this.filteredItems = ko.computed(function() {
         let filter2 = self.filter().toLowerCase();
 
-        if (!filter2){
+        if (!filter2) {
             return self.spotList();
-        }
-        else {
-            return ko.utils.arrayFilter(self.spotList(), function(spot){
-                if ( spot.name.toLowerCase().indexOf(filter2) >= 0) {
+        } else {
+            return ko.utils.arrayFilter(self.spotList(), function(spot) {
+                if (spot.name.toLowerCase().indexOf(filter2) >= 0) {
                     return true;
-                }
-                else {
+                } else {
                     return false;
                 }
             })
@@ -244,7 +246,7 @@ let ViewModel = function(){
     })
 }
 // populates info window whem marker is clicked.
-function populateInfoWindow(marker, infowindow, self){
+function populateInfoWindow(marker, infowindow, self) {
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
         infowindow.setContent('<div>' + self.name + '</div>' +
@@ -254,7 +256,7 @@ function populateInfoWindow(marker, infowindow, self){
             '<div> Swell data provided by magicseaweed.com   </div>');
         infowindow.open(map, marker);
         // clear marker property if info window closed
-        infowindow.addListener('closeclick', function(){
+        infowindow.addListener('closeclick', function() {
             infowindow.setMarker = null;
         });
     }
@@ -265,26 +267,28 @@ function populateInfoWindow(marker, infowindow, self){
 // of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
     let markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(35, 60),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(35,60));
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(35, 60),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(35, 60));
     return markerImage;
 }
 
-function magCopy (urlcopy, spotCopy) {
-    return function (){
+function magCopy(urlcopy, spotCopy) {
+    return function() {
         let f = spotCopy;
         $.ajax({
-          url: urlcopy,
-          dataType: "jsonp",
-          success: function(data){
-              f.swellHeight = data[0].swell.components.primary.height
-              f.swellDirection = data[0].swell.components.primary.direction
-              f.swellPeriod = data[0].swell.components.primary.period
-          }
-        }).fail(function(){alert("Magic SeaWeed API cold not be loaded")});
+            url: urlcopy,
+            dataType: 'jsonp',
+            success: function(data) {
+                f.swellHeight = data[0].swell.components.primary.height
+                f.swellDirection = data[0].swell.components.primary.direction
+                f.swellPeriod = data[0].swell.components.primary.period
+            }
+        }).fail(function() {
+            alert('Magic SeaWeed API cold not be loaded')
+        });
     }
 }
